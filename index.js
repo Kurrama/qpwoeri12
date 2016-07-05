d5ed51a94f43bae685db0c5d3aa0765f4731d0be
Peter <pkashuba@mail.ua>
2016-07-05 16:00:22 +0000

    new add

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-var f = [0,1];
-if (n <= 1000) {
-    var result = f.slice(0,n);
-} else {
-    for (i = 0; i < n - 1000, i++) {
-        f.push(f[f.lenght - 1] + [f.lenght - 2]);
-    }
-    var result = f;
-}
-alert (result);
+// JavaScript File
